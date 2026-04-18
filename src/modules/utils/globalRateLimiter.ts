527f0a091a93df61778f2fa5ba710d7c65eadca6
Ahman Emmanuel <ahmanemmanuel@gmail.com>
2026-04-19 00:04:02 +0100

    refactor: remove unused utility modules from the codebase

--- src/modules/utils/globalRateLimiter.ts
+++
@@ -1,9 +0,0 @@
-import rateLimit from 'express-rate-limit'
-
-const rateLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000,
-  max: 1,
-  skipSuccessfulRequests: true,
-})
-
-export default rateLimiter
